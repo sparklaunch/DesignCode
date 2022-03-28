82a8b5256142e745b79854f3d619a3930fbc0a59
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-28 10:35:30 +0900

    Add the card stack.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,15 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text("UI Design")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                Text("Certificate")
-                    .foregroundColor(Color("accent"))
+        ZStack {
+            VStack {
+                Spacer()
             }
-            Image("Logo1")
+            .frame(width: 300, height: 220)
+            .background(.blue)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding()
+                Spacer()
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 300, height: 110, alignment: .top)
+            }
+            .frame(width: 340, height: 220)
+            .background(.black)
+            .cornerRadius(20)
+        .shadow(radius: 20)
         }
     }
 }
