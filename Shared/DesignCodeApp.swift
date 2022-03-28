6af091fe31e9da4e021871f39b25be42c3e2aeaa
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-28 09:54:04 +0900

    Initial Commit

---
+++ Shared/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
