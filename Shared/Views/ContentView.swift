93a050f368c30061590b364af69ef5fd1ec84f67
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-29 12:12:48 +0900

    Finish the bottom card view.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -31,15 +31,7 @@ struct ContentView: View {
                 .blendMode(.hardLight)
             CardView()
                 .blendMode(.hardLight)
-            VStack {
-                Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor.")
-                Spacer()
-            }
-            .padding()
-            .background(.white)
-            .cornerRadius(30)
-            .shadow(radius: 20)
-            .offset(x: 0, y: 500)
+            BottomCardView()
         }
     }
 }
@@ -101,5 +93,28 @@ struct TitleView: View {
             Image("Background1")
             Spacer()
         }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor.")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+        .background(.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(x: 0, y: 500)
     }
 }
