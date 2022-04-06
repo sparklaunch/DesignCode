c48758b15a800d59c94f078d47508cac81b851ff
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-06 11:58:22 +0900

    Decorate the menu view.

--- Shared/Views/MenuView.swift
+++ Shared/Views/MenuView.swift
@@ -12,6 +12,19 @@ struct MenuView: View {
         VStack {
             Spacer()
             VStack(spacing: 16) {
+                Text("Meng - 28% complete")
+                    .font(.caption)
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .shadow(color: .black.opacity(0.2), radius: 5, x: .zero, y: 5)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(.black.opacity(0.08))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(.black.opacity(0.1))
+                    .cornerRadius(12)
                 MenuRow(title: "Account", icon: "gear")
                 MenuRow(title: "Billing", icon: "creditcard")
                 MenuRow(title: "Sign Out", icon: "person.crop.circle")
@@ -20,8 +33,16 @@ struct MenuView: View {
             .frame(height: 300)
             .background(LinearGradient(colors: [.white, Color("GradientColor")], startPoint: .top, endPoint: .bottom))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(radius: 30)
+            .shadow(color: .black.opacity(0.2), radius: 20, x: .zero, y: 20)
             .padding(.horizontal, 30)
+            .overlay(
+                Image(decorative: "Avatar")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            )
         }
         .padding(.bottom, 30)
     }
@@ -42,6 +63,7 @@ struct MenuRow: View {
                 .font(.system(size: 20, weight: .light))
                 .imageScale(.large)
                 .frame(width: 32, height: 32)
+                .foregroundColor(Color("IconColor"))
             Text(title)
                 .font(.system(size: 20, weight: .bold, design: .default))
                 .frame(width: 120, alignment: .leading)
