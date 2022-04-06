a3afa9a16a11d3b8a442ef53056a517de9f890a5
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-06 13:13:15 +0900

    Add the menu view transition effects.

--- Shared/Views/Home.swift
+++ Shared/Views/Home.swift
@@ -11,6 +11,8 @@ struct Home: View {
     @State private var showProfile: Bool = false
     var body: some View {
         ZStack {
+            Color(.lightGray)
+                .edgesIgnoringSafeArea(.all)
             VStack {
                 HStack {
                     Text("Watching")
@@ -31,6 +33,15 @@ struct Home: View {
                 .padding(.top, 30)
                 Spacer()
             }
+            .background(
+                Color .white
+                    .edgesIgnoringSafeArea(.all)
+            )
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous ))
+            .offset(y: showProfile ? -450 : .zero)
+            .shadow(color: .black.opacity(0.2), radius: 20, x: .zero, y: 20)
+            .rotation3DEffect(Angle(degrees: showProfile ? -10 : .zero), axis: (x: 10, y: .zero, z: .zero))
+            .scaleEffect(showProfile ? 0.9 : 1.0)
             MenuView()
                 .offset(y: showProfile ? .zero : 600)
         }
